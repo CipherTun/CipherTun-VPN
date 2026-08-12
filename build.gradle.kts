224688b6145cdd8121ff332d18b5407cb1c23013
CipherTun <surprisekhoza04@gmail.com>
2026-08-12 21:27:11 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
 
-    id("com.google.devtools.ksp") version "2.3.10" apply false
-    id("com.github.triplet.play") version "4.0.0" apply false
+  id("com.android.application") version "9.4.0-alpha06" apply false
+id("com.android.library") version "9.4.0-alpha06" apply false
 
     alias(libs.plugins.spotless) apply false
 
