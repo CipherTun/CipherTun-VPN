d914a66f4fe9e9ef758a6e40e0eb097d9a53eb2b
CipherTun <surprisekhoza04@gmail.com>
2026-09-23 11:25:45 +0200

    fix: remove duplicate material api import

--- app/src/main/java/io/surprise/ciphertun/compose/screen/tools/ToolsScreen.kt
+++ app/src/main/java/io/surprise/ciphertun/compose/screen/tools/ToolsScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.outlined.NetworkCheck
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.SwapHoriz
 import androidx.compose.material.icons.outlined.Tune
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.CardDefaults
